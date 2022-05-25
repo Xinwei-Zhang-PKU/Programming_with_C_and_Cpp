09bee5329e331b3e6db351fdf11a82fd966d7a4d
Xinwei_Zhang <103341916+Xinwei-Zhang-PKU@users.noreply.github.com>
2022-05-25 10:45:30 +0800

    Update pie.cpp

--- enumerate_and_binary_search/pie.cpp
+++ enumerate_and_binary_search/pie.cpp
@@ -1,1 +1,45 @@
+//http://cxsjsx.openjudge.cn/hw202212/C/
+#include<iostream>
+#include<cmath>
+#include<algorithm>
+#include<iomanip>
 
+const double pi = M_PI;
+const double eps = 1e-5;
+const int kMaxN = 10010;
+int N, F;
+double v[kMaxN];
+double res = 0;
+
+bool check(double x) {
+    int num = 0;
+    for (int i = 0; i < N; ++i) {
+        num += static_cast<int>(v[i] / x);
+    }
+    if (num >= F)
+        return true;
+    else
+        return false;
+}
+
+int main() {
+    std::cin >> N >> F;
+    ++F;
+    for (int i = 0; i < N; ++i) {
+        int x;
+        std::cin >> x;
+        v[i] = pi * x * x;
+    }
+    double sta = 0, end = *std::min_element(v, v + N);
+    while (sta + eps < end) {
+        double tmp = sta + (end - sta) / 2;
+        if (check(tmp) && res < tmp) {
+            res = tmp;
+            sta = tmp;
+        } else {
+            end -= eps;
+        }
+    }
+    std::cout << std::setiosflags(std::ios::fixed) << std::setprecision(3);
+    std::cout << res << std::endl;
+}
